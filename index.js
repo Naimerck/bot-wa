d2ad1769d8a0f5f8218aebb44e141060c7ba1aa8
Naimerck <garciasian99@gmail.com>
2026-04-22 22:47:01 -0300

    Add Express server with a basic route

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ async function startBot() {
 }
 
 startBot()
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot activo");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Running on port " + PORT));
